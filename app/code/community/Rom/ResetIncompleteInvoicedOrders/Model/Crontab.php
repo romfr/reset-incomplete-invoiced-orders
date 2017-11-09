de2a9faf756e4fa5757ee475dd49ec171df32f35
Neromes | Prod | Hetzner 3 <romprodh3@rom.fr>
2017-11-09 12:13:05 +0100

    Reset order and order items invoice flags

--- app/code/community/Rom/ResetIncompleteInvoicedOrders/Model/Crontab.php
+++ app/code/community/Rom/ResetIncompleteInvoicedOrders/Model/Crontab.php
@@ -2,6 +2,32 @@
 class Rom_ResetIncompleteInvoicedOrders_Model_Crontab
 {
     public function runReset($schedule = null) {
-        
+        $this->resetMissingInvoices();
+    }
+
+    protected function resetMissingInvoices() {
+        $readConnection = Mage::getSingleton('core/resource')->getConnection('core_read');
+        $writeConnection = Mage::getSingleton('core/resource')->getConnection('core_write');
+        $query = 'SELECT sfo.entity_id as order_id, sfo.increment_id, sfo.created_at, sfo.updated_at, sfo.grand_total'
+            . ' FROM sales_flat_order sfo'
+            . ' LEFT JOIN sales_flat_invoice sfi'
+            . ' ON sfo.entity_id = sfi.order_id'
+            . ' WHERE sfi.grand_total IS NULL AND sfo.total_invoiced > 0 AND sfo.status <> "complete"'
+            . ' ORDER BY sfo.created_at DESC';
+        foreach ($readConnection->fetchAll($query) as $orderToFix) {
+            $orderResetInvoicedFlagsQuery = 'UPDATE sales_flat_order'
+                . ' SET base_discount_invoiced = null, base_shipping_invoiced = null, base_subtotal_invoiced = null,'
+                . ' base_tax_invoiced = null, base_total_invoiced = null, base_total_invoiced_cost = null,'
+                . ' base_total_paid = null, discount_invoiced = null, shipping_invoiced = null, '
+                . ' hidden_tax_invoiced = null, base_hidden_tax_invoiced = null'
+                . ' WHERE entity_id = '.$orderToFix['order_id'];
+            $orderItemsResetInvoicedFlagsQuery = 'UPDATE sales_flat_order_item'
+                . ' SET qty_invoiced = 0, tax_invoiced = null, base_tax_invoiced = null, discount_invoiced = null,'
+                . ' base_discount_invoiced = null, row_invoiced = null, base_row_invoiced = null,'
+                . ' hidden_tax_invoiced = null, base_hidden_tax_invoiced = null'
+                . ' WHERE order_id = '.$orderToFix['order_id'];
+            $writeConnection->query($orderResetInvoicedFlagsQuery);
+            $writeConnection->query($orderItemsResetInvoicedFlagsQuery);
+        }
     }
 }
